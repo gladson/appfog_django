96920c6bf6f45e76e86bfb788bad5047862bb488
unknown <gladsonbrito@gmail.com>
2012-09-12 03:06:57 -0400

    segundo

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
-# Django settings for a generic project.
+from wsgi import *
+from django.contrib.auth.models import User
+
 import os
 PASTA = os.path.dirname(__file__)
 
@@ -40,30 +42,16 @@ else:
             }
         }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'America/Porto_Velho'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'pt-BR'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
 
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
 MEDIA_ROOT = os.path.join(PASTA, 'media')
@@ -80,18 +68,14 @@ STATICFILES_DIRS = (
     os.path.join(PASTA, 'sitestatic'),
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = '0tc6gk^8x=lfzyh0&amp;%1u^7tu0wb(aho7o6+6!*yr!=#c#b4c$@'
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
@@ -119,32 +103,39 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'urls'
 
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'wsgi.application'
+
+#Criar Usuario
+u, created = User.objects.get_or_create(username='admin')
+if created:
+    u.set_password('P@s$w0rd1')
+    u.is_superuser = True
+    u.is_staff = True
+    u.save()
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, "templates"),
 )
 
-INSTALLED_APPS = (
+DJANGO_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
+    'django.contrib.staticfiles',    
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+ADD_APPS = (
+    'django.contrib.admin',
+)
+
+PROJETO_APPS = (
+)
+
+INSTALLED_APPS = DJANGO_APPS + ADD_APPS + PROJETO_APPS
+
 # LOGGING = {
 #     'version': 1,
 #     'disable_existing_loggers': False,
